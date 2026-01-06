018fe2406cf04f1ab28c6d2128da552965359cd0
Miguelkoro_PC\Miguelkoro <miguelkoro@gmail.com>
2026-01-06 16:42:18 +0100

    no message

--- GameManager.gd
+++ GameManager.gd
@@ -2,7 +2,7 @@ extends Node
 #Para cuando recorre el camino y llega al final
 func finished_walking() -> void:
 	var scene = get_tree().get_first_node_in_group("Scene")
-	print("terminao")
+	#print("terminao")
 	scene.finished_walking()
 	
 func no_reachable() ->void:
