09e2d50ea54d4a4b000cbd1a999736bbc070d201
Miguelkoro_PC\Miguelkoro <miguelkoro@gmail.com>
2026-01-15 21:52:50 +0100

    añadidos algunos comentarios

--- player.gd
+++ player.gd
@@ -13,21 +13,21 @@ func _physics_process(delta: float) -> void:
 		navigation_agent_2d.is_target_reached(),
 		navigation_agent_2d.is_target_reachable()
 	]
-	if navigation_agent_2d.is_target_reached():
+	if navigation_agent_2d.is_target_reached(): #Si se ha alcanzado el objetivo se ejecuta el finishied.walking del singleton
 		GameManager.finished_walking()
 	elif not navigation_agent_2d.is_target_reachable() and navigationBool and navigation_agent_2d.is_navigation_finished(): 
-		GameManager.no_reachable()
+		GameManager.no_reachable() #Si ha acabado de navegar y no se alcanza el objetivo, se ejecuta el metodono_reachable
 		navigationBool = false
 	#manual_navigation()
 	navigate(delta)
 
 func manual_navigation(position: Vector2) -> void:
 	if Input.is_action_just_pressed("set_target"):
-		navigation_agent_2d.target_position = position
-		navigationBool = true
+		navigation_agent_2d.target_position = position #Añade la posicion al navegador 2d
+		navigationBool = true #indica que ha empezado a navegar
 
-func navigate(delta: float) -> void:
-	if navigation_agent_2d.is_navigation_finished():
+func navigate(delta: float) -> void: #navega al punto que le hemos puesto
+	if navigation_agent_2d.is_navigation_finished(): #Si ha terminado de navegar
 		return
 	var next_path_position: Vector2 = navigation_agent_2d.get_next_path_position()
 	var new_velocity: Vector2 = (global_position.direction_to(next_path_position)* speed)
--- tile_map_layer.gd
+++ tile_map_layer.gd
@@ -2,17 +2,19 @@ extends TileMapLayer
 @export var noise: FastNoiseLite
 @export var sc: float = 0.1
 @export var height_sc: float = 10
+@export var mapWidth: float = 100
+@export var mapHeight: float = 100
 
 #@export var atlas_arr = [Vector2i(1,1),Vector2i(7,1),Vector2i(12,1),Vector2i(7,5)]
-@export var atlas_arr = [Vector2i(7,9),Vector2i(18,5),Vector2i(17,6)]
+@export var atlas_arr = [Vector2i(7,9),Vector2i(18,5),Vector2i(17,6)] #Se eligen las tiles del tileSet
 
 func _ready() -> void:
 	create_map()
 	
 func create_map() -> void:
 	noise.seed = randi() #Crea una semilla aleatoria
-	for i in range(100):
-		for j in range(100):
+	for i in range(mapHeight):
+		for j in range(mapWidth):
 			var x: float = i#randf_range(-50,50)
 			var y: float = j#randf_range(-50,50)
 					
@@ -20,7 +22,7 @@ func create_map() -> void:
 			height = (height+1)/2
 			#var int_height = height as int
 			var vect_to_use: Vector2i  
-			if height < 0.5:
+			if height < 0.5: #Si el numero sacado del ruido es menor que 0.5 pinta el tile 0
 				vect_to_use=atlas_arr[0]
 			elif height < 0.75:
 				vect_to_use=atlas_arr[1]
@@ -29,4 +31,4 @@ func create_map() -> void:
 			else:
 				vect_to_use=atlas_arr[2]
 			#var newNode := BALL.instantiate()	
-			set_cell(Vector2i(x,y),0,vect_to_use)
+			set_cell(Vector2i(x,y),0,vect_to_use) #Pinta la celda con el tileset elegido segun el ruido
